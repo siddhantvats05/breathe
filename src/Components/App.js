4e8f4733a5d1f323240de14290785db6117616da
Siddhant Vats <98140422+siddhantvats05@users.noreply.github.com>
2022-04-21 22:15:29 +0530

    Add files via upload

---
+++ src/Components/App.js
@@ -0,0 +1,24 @@
+import './App.css';
+import Header from './Header';
+import HomePage from './HomePage';
+import Questionnaire from './Questionnaire';
+import Symptom from './Symptom';
+import Resources from './Resources';
+import ContactUs from './ContactUs'
+import Footer from './Footer'
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <HomePage />
+      <Questionnaire />
+      <Symptom />
+      <Resources />
+      <ContactUs />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Footer.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div id="footer">
+        <div className="breathe-logo">
+            <h1>Breathe</h1>
+        </div>
+
+        <nav id="footer-nav">
+            <a href="#" alt="terms-button">Terms</a>
+            <a href="#contact-us" alt="contact-us-button">Contact</a>
+            <a href="#" alt="about-button">About</a>
+            <a href="#home-page" alt="home-button">Home</a>
+            <a href="#" alt="get-help-button">Get Help</a>
+            <a href="#resources" alt="resources-button">Resources</a>
+            <a href="#contact-us" alt="call-us-button">Call Us</a>
+        </nav>
+    </div>
+  )
+}
+
+export default Footer
